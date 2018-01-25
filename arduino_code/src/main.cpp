0bf7177aa1180cefc4d963d8746d4917a358ceff
Raphael Mader <RaphMad@users.noreply.github.com>
2018-01-26 00:47:51 +0100

    Played around with code (not functional yet)

--- arduino_code/src/main.cpp
+++ arduino_code/src/main.cpp
@@ -1,7 +1,27 @@
 #include <Arduino.h>
 
+// pin to button mapping
+const uint8_t PIN_BTN_B = 4;
+const uint8_t PIN_BTN_Y = 5;
+const uint8_t PIN_BTN_SELECT = 6;
+const uint8_t PIN_BTN_START = 7;
+const uint8_t PIN_BTN_UP = 8;
+const uint8_t PIN_BTN_DOWN = 9;
+const uint8_t PIN_BTN_L = 10;
+const uint8_t PIN_BTN_R = 11;
+const uint8_t PIN_BTN_A = 12;
+
+const uint8_t PIN_BTN_X = A0;
+const uint8_t PIN_BTN_SHOULDER_LEFT = A1;
+const uint8_t PIN_BTN_SHOULDER_LEFT = A2;
+
+// clock line is attached to an interrupt pin
+const uint8_t PIN_CLOCK = 2;
+
+uint8_t clockCounter = 0;
+
 void setup() {
-    // put your setup code here, to run once:
+    pulseCounter++;
 }
 
 void loop() {
