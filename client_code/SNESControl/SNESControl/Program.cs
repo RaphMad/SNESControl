4da0553aecb00053d460dab94fc12805870365f5
Raphael Mader <RaphMad@users.noreply.github.com>
2018-01-27 22:30:13 +0100

    Cleaned up user interface

--- client_code/SNESControl/SNESControl/Program.cs
+++ client_code/SNESControl/SNESControl/Program.cs
@@ -7,11 +7,7 @@
 
     class Program
     {
-        ////private static FileStream _saveToFile;
-
-        ////private static List<byte> _readFromFileBytes;
-
-        ////private static int _readFromFileIndex;
+        private static SerialConnector _serialConnector;
 
         static void Main(string[] args)
         {
@@ -24,96 +20,54 @@
                 return;
             }
 
-            ////int saveToFileIndex = argList.FindIndex(x => x == "-saveToFile");
+            _serialConnector = new SerialConnector(argList[1]);
+            _serialConnector.Start();
 
-            ////if (saveToFileIndex != -1)
-            ////{
-            ////    _saveToFile = File.Open(argList[saveToFileIndex + 1], FileMode.Append, FileAccess.Write);
-            ////}
+            Console.WriteLine("Commands:");
+            Console.WriteLine();
+            Console.WriteLine("'i': request information from backend");
+            Console.WriteLine("'p': send a PING request containing 256 bytes");
+            Console.WriteLine("'ESC': quit");
+            Console.WriteLine();
 
-            ////int readFromFileIndex = argList.FindIndex(x => x == "-loadFromFile");
+            ProcessKeys();
 
-            ////if (readFromFileIndex != -1)
-            ////{
-            ////    string content = File.ReadAllText(args[readFromFileIndex + 1]);
-            ////    string[] textualBytes = content.Split(' ', '\r', '\n');
-            ////    _readFromFileBytes = textualBytes.Where(x => !string.IsNullOrEmpty(x)).Select(parseByte).ToList();
-            ////}
+            _serialConnector.Stop();
 
-            var serialConnector = new SerialConnector(argList[1]);
-            serialConnector.Start();
-
-            Console.WriteLine("Press any key to send some bytes.");
-            Console.ReadKey();
-
-            byte[] bytes = Enumerable.Range(0, 64).Select(x => (byte)x).ToArray();
-            serialConnector.SendData(MessageType.Ping, bytes);
-
-            Console.WriteLine("Press any key to request data.");
-            Console.ReadKey();
-
-            serialConnector.SendData(MessageType.RequestStatus, new byte[]{});
-
-            Console.WriteLine("Press any key to quit");
-            Console.ReadKey();
-
-            serialConnector.Stop();
-
-            // wait some time to allow the file handles to be closed
+            // wait some time to allow for cleanup 
             Thread.Sleep(1000);
         }
 
-        ////static byte parseByte(string input)
-        ////{
-        ////    return byte.Parse(input);
-        ////}
+        static void ProcessKeys()
+        {
+            ConsoleKey pressedKey = ConsoleKey.OemClear;
 
-        ////static void ProcessSerialInput()
-        ////{
-        ////    _port.Open();
+            while (pressedKey != ConsoleKey.Escape)
+            {
+                pressedKey = Console.ReadKey(false).Key;
 
-        ////    if (_readFromFileBytes != null)
-        ////    {
-        ////        _port.WriteLine("ENABLE REPLAY");
-        ////    }
+                var keyActions = new Dictionary<ConsoleKey, Action>
+                                 {
+                                     { ConsoleKey.I, RequestInfo },
+                                     { ConsoleKey.P, SendPing }
+                                 };
 
-        ////    if (_saveToFile != null)
-        ////    {
-        ////        _port.WriteLine("ENABLE SAVE");
-        ////    }
+                if (keyActions.ContainsKey(pressedKey))
+                {
+                    keyActions[pressedKey]();
+                }
+            }
+        }
 
-        ////    while (_processSerialInput)
-        ////    {
-        ////        string nextCommand = _port.ReadLine();
+        static void RequestInfo()
+        {
+            _serialConnector.SendData(MessageType.RequestStatus, new byte[] { });
+        }
 
-        ////        if (nextCommand.StartsWith("SAVE|"))
-        ////        {
-        ////            if (_saveToFile != null)
-        ////            {
-        ////                string data = nextCommand.Substring(5) + Environment.NewLine;
-        ////                byte[] bytes = Encoding.ASCII.GetBytes(data);
-        ////                _saveToFile.Write(bytes, 0, bytes.Length);
-        ////            }
-        ////        }
-        ////        else if (nextCommand.StartsWith("LOAD|"))
-        ////        {
-        ////            int amountOfBytes = int.Parse(nextCommand.Substring(5));
-        ////            if (_readFromFileBytes != null && _readFromFileIndex < _readFromFileBytes.Count - amountOfBytes)
-        ////            {
-        ////                byte[] nextBytes = new byte[amountOfBytes];
-        ////                _readFromFileBytes.CopyTo(0, nextBytes, _readFromFileIndex, amountOfBytes);
-
-        ////                _port.Write(nextBytes, 0, 2);
-        ////                _readFromFileIndex += amountOfBytes;
-        ////            }
-        ////        }
-        ////        else
-        ////        {
-        ////            Console.WriteLine("Received incomplete line: " + nextCommand + " " + DateTime.Now.Millisecond);
-        ////        }
-        ////    }
-
-        ////    _saveToFile?.Dispose();
-        ////}
+        static void SendPing()
+        {
+            byte[] bytes = Enumerable.Range(0, 256).Select(x => (byte)x).ToArray();
+            _serialConnector.SendData(MessageType.Ping, bytes);
+        }
     }
 }
