54b290a1166adec38e3d2eaf2ed7b40d0c9ae604
Raphael Mader <RaphMad@users.noreply.github.com>
2018-01-30 19:05:36 +0100

    Optimized order, added some forward declarations

--- arduino_code/src/main.cpp
+++ arduino_code/src/main.cpp
@@ -6,6 +6,10 @@
 #include "StoreButtonData/StoreButtonData.h"
 #include "WriteToConsole/WriteToConsole.h"
 
+/*
+ * Holds global information about the application.
+ */
+AppInfo appInfo;
 
 /*
  * Expected frame length in ms.
@@ -14,19 +18,15 @@
 static const uint8_t FRAME_LENGTH = 20;
 
 /*
- * Holds global information about the application.
- */
-AppInfo appInfo;
-
-/*
  * Indicates whether we are right after a latch.
  */
 static volatile bool isAfterLatch = false;
 
-void handleFallingLatchPulse() {
-    // keep interrupt handler short, actual code will be executed in main loop
-    isAfterLatch = true;
-}
+void handleFallingLatchPulse();
+void calculateLatchInfo();
+void checkButtonTiming(uint16_t);
+void pollController();
+void calculateLoopDuration();
 
 void setup() {
     attachInterrupt(digitalPinToInterrupt(PIN_LATCH), handleFallingLatchPulse, FALLING);
@@ -37,17 +37,32 @@ void setup() {
     WriteToConsole::begin();
 }
 
-void calculateLoopDuration() {
-    static uint16_t lastLoop;
+void handleFallingLatchPulse() {
+    // keep interrupt handler short, actual code will be executed in main loop
+    isAfterLatch = true;
+}
 
-    uint16_t timeNow = millis();
-    uint16_t loopDuration = timeNow - lastLoop;
+void loop() {
+    if (isAfterLatch) {
+        isAfterLatch = false;
 
-    if (loopDuration > appInfo.maxLoopDuration) {
-        appInfo.maxLoopDuration = loopDuration;
+        calculateLatchInfo();
+
+        if (appInfo.isInSaveMode) {
+            StoreButtonData::storeData(WriteToConsole::getLatestData());
+        }
+
+        if (appInfo.isInReplayMode) {
+            ButtonData buttonData = LoadButtonData::getData();
+            checkButtonTiming(buttonData.pressedAt);
+
+            WriteToConsole::prepareData(buttonData);
+        }
     }
 
-    lastLoop = timeNow;
+    pollController();
+    Messenger::checkForData();
+    calculateLoopDuration();
 }
 
 void calculateLatchInfo() {
@@ -68,6 +83,23 @@ void calculateLatchInfo() {
     lastLatch = timeNow;
 }
 
+void checkButtonTiming(uint16_t pressedAt) {
+    uint16_t timeNow = millis();
+    uint16_t timeFromFirstLatch = timeNow - appInfo.firstLatch;
+
+    int8_t buttonDelay = pressedAt - timeFromFirstLatch - FRAME_LENGTH;
+
+    if (buttonDelay < -FRAME_LENGTH) {
+        // loading the next button data effectively skips a frame
+        LoadButtonData::getData();
+        appInfo.skipCount++;
+    } else if (buttonDelay > FRAME_LENGTH / 2) {
+        // delay a bit so we can sync up
+        delay(buttonDelay);
+        appInfo.delayCount++;
+    }
+}
+
 void pollController() {
     // Controller will be polled twice per frame.
     static uint16_t lastPoll;
@@ -81,43 +113,15 @@ void pollController() {
     }
 }
 
-void checkButtonTiming(ButtonData* buttonData) {
-    uint16_t buttonTime = buttonData->pressedAt;
+void calculateLoopDuration() {
+    static uint16_t lastLoop;
+
     uint16_t timeNow = millis();
-    uint16_t timeFromFirstLatch = timeNow - appInfo.firstLatch;
+    uint16_t loopDuration = timeNow - lastLoop;
 
-    int8_t buttonDelay = buttonTime - timeFromFirstLatch - FRAME_LENGTH;
-
-    if (buttonDelay < -FRAME_LENGTH) {
-        // loading the next button data effectively skips a frame
-        LoadButtonData::getData();
-        appInfo.skipCount++;
-    } else if (buttonDelay > FRAME_LENGTH / 2) {
-        // delay a bit so we can sync up
-        delay(buttonDelay);
-        appInfo.delayCount++;
-    }
-}
-
-void loop() {
-    if (isAfterLatch) {
-        isAfterLatch = false;
-
-        calculateLatchInfo();
-
-        if (appInfo.isInSaveMode) {
-            StoreButtonData::storeData(WriteToConsole::getLatestData());
-        }
-
-        if (appInfo.isInReplayMode) {
-            ButtonData buttonData = LoadButtonData::getData();
-            checkButtonTiming(&buttonData);
-
-            WriteToConsole::prepareData(buttonData);
-        }
+    if (loopDuration > appInfo.maxLoopDuration) {
+        appInfo.maxLoopDuration = loopDuration;
     }
 
-    pollController();
-    Messenger::checkForData();
-    calculateLoopDuration();
+    lastLoop = timeNow;
 }
