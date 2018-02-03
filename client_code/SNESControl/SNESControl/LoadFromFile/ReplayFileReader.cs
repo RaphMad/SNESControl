c08ee23b06a959884b38a6bef1eb341b98b0b73d
Raphael Mader <RaphMad@users.noreply.github.com>
2018-02-03 22:15:00 +0100

    Added some prototype functions that may decode external replay file formats

--- client_code/SNESControl/SNESControl/LoadFromFile/ReplayFileReader.cs
+++ client_code/SNESControl/SNESControl/LoadFromFile/ReplayFileReader.cs
@@ -1,6 +1,7 @@
 ﻿namespace SNESControl.SaveToFile
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
 
@@ -23,6 +24,8 @@
             {
                 _currentIndex = 0;
                 _bytesFromFile = File.ReadAllBytes(_fileName);
+                //_bytesFromFile = ConvertFromSnes9xFile(File.ReadAllBytes(_fileName));
+                //_bytesFromFile = ConvertFromLsnesFile(File.ReadAllLines(_fileName));
             }
             catch (Exception e)
             {
@@ -38,6 +41,89 @@
             _currentIndex += numberOfBytes;
 
             return bytes;
+        }
+
+        private byte[] ConvertFromSnes9xFile(byte[] file)
+        {
+            var convertedBytes = new List<byte>();
+
+            uint inputOffset = BitConverter.ToUInt32(file, 0x1C);
+            ushort frameTime = 0;
+
+            for (uint i = inputOffset; i < file.Length; i += 2)
+            {
+                byte byte1 = file[i];
+                byte byte2 = file[i + 1];
+
+                convertedBytes.Add(Reverse((byte)~byte2));
+                convertedBytes.Add(Reverse((byte)~byte1));
+
+                convertedBytes.AddRange(BitConverter.GetBytes(frameTime));
+                frameTime += 16;
+            }
+
+            return convertedBytes.ToArray();
+        }
+
+        // Reverses bits in a byte
+        public static byte Reverse(byte inByte)
+        {
+            byte result = 0x00;
+
+            for (byte mask = 0x80; Convert.ToInt32(mask) > 0; mask >>= 1)
+            {
+                // shift right current result
+                result = (byte)(result >> 1);
+
+                // tempbyte = 1 if there is a 1 in the current position
+                var tempbyte = (byte)(inByte & mask);
+                if (tempbyte != 0x00)
+                {
+                    // Insert a 1 in the left
+                    result = (byte)(result | 0x80);
+                }
+            }
+
+            return (result);
+        }
+
+        private byte[] ConvertFromLsnesFile(string[] file)
+        {
+            var convertedBytes = new List<byte>();
+            ushort frameTime = 0;
+
+            for (int i = 0; i < file.Length; i++)
+            {
+                string currentLine = file[i];
+
+                currentLine = currentLine.Substring(7);
+
+                byte byte1 = 255;
+                byte byte2 = 255;
+
+                if (currentLine.Contains("B")) byte1 &= 0b11111110;
+                if (currentLine.Contains("Y")) byte1 &= 0b11111101;
+                if (currentLine.Contains("s")) byte1 &= 0b11111011;
+                if (currentLine.Contains("S")) byte1 &= 0b11110111;
+                if (currentLine.Contains("u")) byte1 &= 0b11101111;
+                if (currentLine.Contains("d")) byte1 &= 0b11011111;
+                if (currentLine.Contains("l")) byte1 &= 0b10111111;
+                if (currentLine.Contains("r")) byte1 &= 0b01111111;
+
+                if (currentLine.Contains("A")) byte2 &= 0b11111110;
+                if (currentLine.Contains("X")) byte2 &= 0b11111101;
+                if (currentLine.Contains("L")) byte2 &= 0b11111011;
+                if (currentLine.Contains("R")) byte2 &= 0b11110111;
+
+                convertedBytes.Add(byte1);
+                convertedBytes.Add(byte2);
+
+                convertedBytes.AddRange(BitConverter.GetBytes(frameTime));
+                frameTime += 16;
+
+            }
+
+            return convertedBytes.ToArray();
         }
     }
 }
